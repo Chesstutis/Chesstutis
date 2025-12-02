19983551615c7acde96ab5b06b75c64a233bf07e
alex <alexhoule415@gmail.com>
2025-12-01 22:40:07 -0500

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="bg-blue-600 w-full">all</h1>
+  )
+}
+
+export default App
